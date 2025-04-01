30845d47391711e0213af0fb4fac1ac588770300
SatelliteSeasons <satelliteseasons@gmail.com>
2025-04-01 15:30:49 +0200

    4) (f) fix d

--- vector.c
+++ vector.c
@@ -27,6 +27,14 @@ p_s_vector vector_alloc(size_t n) {
 
 // Libère la mémoire du tableau dynamique et met le pointeur à NULL
 VectorStatus vector_free(p_s_vector *p_vector) {
+    // Si le pointeur est NULL, retourne une erreur
+    if (p_vector == NULL) {
+        return VECTOR_ERROR_NULL_POINTER;
+    }
+    // Si le tableau est vide ou de taille 0, retourne une erreur
+    if ((*p_vector)->data == NULL) {
+        return VECTOR_ERROR_NOT_EMPTY;
+    }
     // Vérifie si le pointeur est valide
     if (p_vector != NULL && *p_vector != NULL) {
         // Libère la mémoire du tableau de données
@@ -36,18 +44,6 @@ VectorStatus vector_free(p_s_vector *p_vector) {
         // Met le pointeur à NULL pour éviter les accès ultérieurs
         *p_vector = NULL;
         return VECTOR_SUCCESS;
-    }
-    // Si le pointeur est NULL, retourne une erreur
-    if (p_vector == NULL) {
-        return VECTOR_ERROR_NULL_POINTER;
-    }
-    // Si le tableau est vide, retourne une erreur
-    if ((*p_vector)->data == NULL) {
-        return VECTOR_ERROR_NOT_EMPTY;
-    }
-    // Si la taille est 0, retourne une erreur
-    if ((*p_vector)->size == 0) {
-        return VECTOR_ERROR_NOT_EMPTY;
     }
     return VECTOR_ERROR_NULL_POINTER;
 }
@@ -92,7 +88,46 @@ VectorStatus vector_get(p_s_vector p_vector, ssize_t i, double *pv) {
 
 // Insère une nouvelle valeur v à l'index i du tableau dynamique
 VectorStatus vector_insert(p_s_vector p_vector, ssize_t i, double v) {
-    // À implémenter
+    // Si le pointeur est NULL, retourne une erreur
+    if (p_vector == NULL) {
+        return VECTOR_ERROR_NULL_POINTER;
+    }
+
+    // Si le tableau est vide ou de taille 0, retourne une erreur
+    if (p_vector->data == NULL) {
+        return VECTOR_ERROR_NOT_EMPTY;
+    }
+
+    // Si l'index est hors limites, retourne une erreur
+    if (i < 0 || i > p_vector->size) {
+        return VECTOR_ERROR_OUT_OF_BOUNDS;
+    }
+
+    // Réalloue de la mémoire pour le tableau dynamique
+    double *new_data = realloc(p_vector->data, (p_vector->size + 1) * sizeof(double));
+    
+    // Incrémente la taille du tableau
+    p_vector->size++;
+    if (new_data != NULL) {
+
+        p_vector->data = new_data;
+        // Décale les éléments à partir de l'index i vers la droite afin d'insérer notre valeur
+        for (ssize_t j = p_vector->size-1; j > i; j--) {
+            p_vector->data[j] = p_vector->data[j - 1];
+        }
+        
+        // Insère la nouvelle valeur à l'index i
+        p_vector->data[i] = v;
+        return VECTOR_SUCCESS;
+    } else {
+        return VECTOR_ERROR_ALLOCATION;
+    }
+
+    // Si la réallocation échoue, retourne une erreur
+    if (p_vector->data == NULL) {
+        return VECTOR_ERROR_ALLOCATION;
+    }
+
     return VECTOR_ERROR_NULL_POINTER;
 }
 

948788a30ce25901b19fae0540eb1a645d608f20
m0ii0m <ludovic.regnier.2004@gmail.com>
2025-05-15 17:19:01 +0200

    Ajout des tests unitaires

--- test_unit.c
+++ test_unit.c
@@ -22,21 +22,85 @@ int test_vector_alloc_free() {
     return 0;
 }
 
-// Test d'insertion et de récupération d'éléments (à compléter par les étudiants)
+// Test d'insertion et de récupération d'éléments
 int test_vector_set_get() {
-    // À compléter
+    p_s_vector vec = vector_alloc(5);
+    ASSERT(vec != NULL);
+    ASSERT(vec->size == 5);
+
+    // Vérification de l'insertion de valeurs dans le vecteur
+    for (size_t i = 0; i < vec->size; i++) {
+        ASSERT(vector_set(vec, i, (double)i) == VECTOR_SUCCESS);
+    }
+
+    // Vérification de la récupération des valeurs
+    for (size_t i = 0; i < vec->size; i++) {
+        double value;
+        ASSERT(vector_get(vec, i, &value) == VECTOR_SUCCESS);
+        ASSERT(value == (double)i);
+    }
+
+    // Libération de la mémoire
+    ASSERT(vector_free(&vec) == VECTOR_SUCCESS);
+    ASSERT(vec == NULL);
+
     return 0;
 }
 
-// Test d'insertion et suppression d'éléments (à compléter par les étudiants)
+// Test d'insertion et suppression d'éléments
 int test_vector_insert_erase() {
-    // À compléter
+    p_s_vector vec = vector_alloc(5);
+    ASSERT(vec != NULL);
+    ASSERT(vec->size == 5);
+    // Insertion de valeurs dans le vecteur
+    for (size_t i = 0; i < vec->size; i++) {
+        ASSERT(vector_set(vec, i, (double)i) == VECTOR_SUCCESS);
+    }
+
+    // Insertion de la valeur 10.0 à l'index 2
+    ASSERT(vector_insert(vec, 2, 10.0) == VECTOR_SUCCESS);
+    double value;
+
+    // Vérification de l'insertion
+    ASSERT(vector_get(vec, 2, &value) == VECTOR_SUCCESS);
+    ASSERT(value == 10.0);
+
+    // Suppression de la valeur insérée
+    ASSERT(vector_erase(vec, 2) == VECTOR_SUCCESS);
+
+    // Vérification de la suppression
+    ASSERT(vector_get(vec, 2, &value) == VECTOR_SUCCESS);
+    ASSERT(value == 2.0);
+
+    // Libération de la mémoire
+    ASSERT(vector_free(&vec) == VECTOR_SUCCESS);
+    ASSERT(vec == NULL);
+
     return 0;
 }
 
-// Test de la fonction vector_empty() (à compléter par les étudiants)
+// Test de la fonction vector_empty()
 int test_vector_empty() {
-    // À compléter
+
+    // Test vecteur nul
+    p_s_vector vec = vector_alloc(0);
+    ASSERT(vec != NULL);
+    ASSERT(vec->size == 0);
+    ASSERT(vector_empty(vec) == VECTOR_SUCCESS);
+    // Libération de la mémoire
+    ASSERT(vector_free(&vec) == VECTOR_SUCCESS);
+    ASSERT(vec == NULL);
+
+    // Test vecteur non nul
+    vec = vector_alloc(5);
+    ASSERT(vec != NULL);
+    ASSERT(vec->size == 5);
+    ASSERT(vector_set(vec, 0, 1.0) == VECTOR_SUCCESS);
+    ASSERT(vector_empty(vec) == VECTOR_ERROR_NOT_EMPTY);
+    // Libération de la mémoire
+    ASSERT(vector_free(&vec) == VECTOR_SUCCESS);
+    ASSERT(vec == NULL);
+
     return 0;
 }
 
